7acca04c96f84100b0db383b62bc4d092cab22fa
em <6em.rva@gmail.com>
2018-09-12 17:39:30 -0400

    added files, spotify api key, and npms

---
+++ liri.js
@@ -0,0 +1,7 @@
+// dotenv to read keys
+require("dotenv").config();
+
+// npm variables
+var Spotify = require('node-spotify-api'); 
+var request = require('request');
+var moment = require('moment');
