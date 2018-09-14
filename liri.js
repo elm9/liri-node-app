21357c7d3351c6d1e7decdee0b1a3341ccdf1d60
em <6em.rva@gmail.com>
2018-09-13 20:18:48 -0400

    psudocode

--- liri.js
+++ liri.js
@@ -5,3 +5,15 @@ require("dotenv").config();
 var Spotify = require('node-spotify-api'); 
 var request = require('request');
 var moment = require('moment');
+// code required to read keys.js file stored in a var
+var key = require('./keys.js');
+var spotify = new Spotify(key.spotify);
+
+
+// function "concert-this"
+
+// function "spotify-this-song"
+
+// function "movie-this"
+
+// function "do-what-it-says"
