5de2a8fba6b4733fbe61d4b86ee79bb3b10cbd6e
em <6em.rva@gmail.com>
2018-09-14 09:37:30 -0400

    pulls argument values from node

--- liri.js
+++ liri.js
@@ -9,11 +9,24 @@ var moment = require('moment');
 var key = require('./keys.js');
 var spotify = new Spotify(key.spotify);
 
-
 // function "concert-this"
+function concertThis(artist){
+    if ((process.argv[2]) == "concert-this"){
+        (process.argv[3]) == artist;
+    }};
+
 
 // function "spotify-this-song"
+function spitifyThis(song){
+    if ((process.argv[2]) == "spotify-this-song"){
+        (process.argv[3]) == song;
+    }};
 
 // function "movie-this"
+function movieThis(movie){
+    if ((process.argv[2]) == "movie-this"){
+        (process.argv[3]) == movie;
+    }};
 
 // function "do-what-it-says"
+
